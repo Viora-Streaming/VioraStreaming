62a3ea4b4e7cadfa8d61bcda8298815e48de3958
Добрянський Володимир <volodymyr.dobrianskyi1@gmail.com>
2026-04-23 12:22:36 +0300

    feat: add Dockerfile for Backend

---
+++ viora-streaming-core/src/main/java/org/viora/viorastreamingcore/VioraStreamingCoreApplication.java
@@ -0,0 +1,13 @@
+package org.viora.viorastreamingcore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VioraStreamingCoreApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(VioraStreamingCoreApplication.class, args);
+  }
+
+}
