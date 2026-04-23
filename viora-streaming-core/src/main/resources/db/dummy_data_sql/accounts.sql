44a1c165c2d198f26fc7019a723bcf43699b5ac4
volodymyrDobry <volodymyr.dobrianskyi1@gmail.com>
2026-04-23 16:08:08 +0300

    feat: add migrations, seed data (#24)

---
+++ viora-streaming-core/src/main/resources/db/dummy_data_sql/accounts.sql
@@ -0,0 +1,2 @@
+INSERT INTO public.accounts (enabled, id, login, password) VALUES (true, 1, 'arthur@example.com', '$2a$10$bcU6f4WITNa9GtLfXceDueEbJG6/2QwUgacmTr3O36NVjSjZr/cGS');
+INSERT INTO public.accounts (enabled, id, login, password) VALUES (false, 52, 'dobry9495@gmail.com', '$2a$10$WF5SONiQpoTqDqlb25f2fe2h3iTsaHnBj3UMiJ4a60j5tYLwcQKSK');
